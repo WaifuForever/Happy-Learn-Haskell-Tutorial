8863d66601e1b11bf30dce4d10d7989b437af392
Waifu Forever <itachiut1r4@gmail.com>
2022-05-11 16:24:03 -0300

    add: Average 1

--- project/app/Main.hs
+++ project/app/Main.hs
@@ -1,6 +1,6 @@
 module Main where
 
-import SimpleProduct
+import Average1
 
 main :: IO ()
 main = fun
---
+++ project/src/Average1.hs
@@ -0,0 +1,18 @@
+module Average1
+    ( fun
+    ) where
+-- https://www.beecrowd.com.br/judge/en/problems/view/1005
+
+import Text.Printf
+
+average :: Double -> Double -> Double
+average a b = (a * 3.5 + b * 7.5)/11
+
+readADouble :: IO Double
+readADouble = readLn
+
+fun = do
+    a <- readADouble
+    b <- readADouble
+    printf "MEDIA = %.5f" (average a b)
+    
